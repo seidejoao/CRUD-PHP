ccf1f8c99bbd4632c6ccae2981976c5a8b384570
João Victor Seide <seidejvs@gmail.com>
2025-04-13 11:36:33 -0300

    Criei o formulário que será o create

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Signup</h3>
+    <form action="includes/databaseHandler.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <input type="text" name="email" placeholder="Email">
+        <button>Signup</button>
+    </form>
+</body>
+</html>
